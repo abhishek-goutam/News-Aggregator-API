be335300b930827344fa2f45e1a5ac82bd36428d
Abhishek Goutam <abhishekgoutam8894@gmail.com>
2024-05-22 07:18:48 +0530

    news aggregator

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/news',)
+
+
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('Something bad happened', err);
+    }
+    console.log(`Server is listening on ${port}`);
+});
+
+
+
+module.exports = app;
